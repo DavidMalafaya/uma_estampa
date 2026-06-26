2fc3ba594d2689c5dd8459e3e1f227136803176d
David Malafaya <147170637+DavidMalafaya@users.noreply.github.com>
2026-06-26 14:00:55 +0000

    Pagina Meus Designs

---
+++ src/app/meus-designs/meus-designs.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meus-designs',
+  templateUrl: './meus-designs.page.html',
+  styleUrls: ['./meus-designs.page.scss'],
+})
+export class MeusDesignsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
